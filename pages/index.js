fc53d4c996011758a90bd8b32b1cb8eaf10d422f
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2024-10-08 20:27:47 +0900

    Header Component done

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
 
+//Internal Import
+import {
+  Header,
+  HeroSection,
+  Footer,
+  Pools,
+  PoolsModel,
+  WithdrawModal,
+  Withdraw,
+  Partners,
+  Statistics,
+  Token,
+  Loader,
+  Notification,
+  ICOSale,
+  Contact,
+  Ask,
+} from "../Components/index";
+
+import {
+  CONTRACT_DATA,
+  deposit,
+  withdraw,
+  claimReward,
+  addTokenMetamask,
+} from "../Context/index";
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <Header /> <Footer />
+    </div>
+  );
 };
 
 export default index;
